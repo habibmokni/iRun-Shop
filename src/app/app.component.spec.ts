762ea4625422aa8cf4675373cddf5d668a814799
Habib Mokni <habib.mokni@check24.de>
2026-02-11 13:21:20 +0100

    RouterTestingModule migration

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,16 +1,12 @@
+import { RouterModule } from '@angular/router';
 import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterModule],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
